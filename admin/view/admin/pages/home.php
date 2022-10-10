bcc4bf4950912cfadfac44e9a784a0d90b6c65c7
hungnguyen251 <102758735+hungnguyen251@users.noreply.github.com>
2022-10-10 23:02:21 +0700

    update admin page

--- admin/view/admin/pages/home.php
+++ admin/view/admin/pages/home.php
@@ -1,10 +1,13 @@
 <?php 
 require('./../layouts/header.php');
+if (!isset($_SESSION)) { 
+    session_start(); 
+} 
 
 if (!isset($_SESSION['account'])) {
     header('Location:login.php');
 } else {
-    header('Location:home.php');
+    header('Location:dashboard.php');
 }
 
 require('./../layouts/footer.php');
